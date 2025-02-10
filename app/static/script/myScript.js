a0f9d84bd83db2d987b5340def567a524ab7d61b
SIMONNEAU Lucas <lucas.simonneau@telecomnancy.eu>
2025-02-10 18:02:34 +0100

    add try catch on selectors, have to test it

--- app/static/script/myScript.js
+++ app/static/script/myScript.js
@@ -8,19 +8,39 @@ async function myFunction(){
     const json = await response.json();
     
     const content = document.querySelector("#text");
-    content.innerHTML = json.data.content;
+    try {
+        content.innerHTML = json.data.content;
+    } catch {
+        throw new Error("JSON error in field json.data.content\n Got " + json.data.content);
+    }
     
     const left_img = document.querySelector("#p1");
-    left_img.innerHTML = json.data.left_img;
+    try {
+        left_img.innerHTML = json.data.left_img;
+    } catch {
+        throw new Error("JSON error in field json.data.left_img\n Got " + json.data.left_img);
+    }
+
 
     const right_img = document.querySelector("#p2");
-    right_img.innerHTML = json.data.right_img;
+    try {
+        right_img.innerHTML = json.data.right_img;
+    } catch {
+        throw new Error("JSON error in field json.data.right_img\n Got " + json.data.right_img);
+    }
 
     const speaker = document.querySelector("#speaker");
-    speaker.innerHTML = json.data.speaker;
-
+    try {
+        speaker.innerHTML = json.data.speaker;
+    } catch {
+        throw new Error("JSON error in field json.data.speaker\n Got " + json.data.speaker);
+    }
     const background = document.getElementsByClassName("body").background-image;
-    background = url(json.data.background);
+    try {
+        background = url(json.data.background);
+    } catch {
+        throw new Error("JSON error in field json.data.background\n Got " + json.data.background);
+    }
     number = json.next[0].to;
     console.log(number);
 }
