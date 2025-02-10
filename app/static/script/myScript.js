985b56363fa71ba20a0e4d0c45240905fcf9c001
SIMONNEAU Lucas <lucas.simonneau@telecomnancy.eu>
2025-02-10 17:40:40 +0100

    set up to change automatically the story

--- app/static/script/myScript.js
+++ app/static/script/myScript.js
@@ -1,9 +1,13 @@
 async function myFunction(){
-    const response = await fetch("http://127.0.0.1:5000/api/stories/nodes/1");
+    const url = "http://127.0.0.1:5000/api/stories/nodes/" + String(number);
+    console.log(url);
+    const response = await fetch(url);
     if(!response.ok) {
         throw new Error("HTTP error " + response.status);
     }
     const json = await response.json();
     const element = document.querySelector("#text");
     element.innerHTML = json.data.content;
+    number = json.next[0].to;
+    console.log(number);
 }
