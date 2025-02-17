0e73977e8f6ee6e8ecb4de056320a4b74d6751c8
Théophile PICHARD <theophile.pichard@telecomnancy.eu>
2025-02-17 09:14:15 +0100

    feat(login): implemented a login page to handle user accounts

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.routes import home_routes
+from app.routes import home_routes, login_routes
 from app.api import api_bp
 from app.extensions import db, bcrypt
 
@@ -7,9 +7,13 @@ def create_app():
     app = Flask(__name__)
     # register the db
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+    # set the secret key
+    app.secret_key = "super secret for now (no)"
     
     # Register blueprints
     app.register_blueprint(home_routes.bp)
+    app.register_blueprint(login_routes.bp)
     app.register_blueprint(api_bp, url_prefix="/api")
 
     bcrypt.init_app(app)
--- app/services/users_service.py
+++ app/services/users_service.py
@@ -1,5 +1,5 @@
 from app.models import User
-from app.extensions import bcrypt
+from app.extensions import bcrypt, db
 
 
 def get_user_by_id(user_id: int) -> object:
@@ -16,9 +16,25 @@ def update_user_progress(user_id: int, story_node_id: int) -> object:
 def create_user(username: str, password: str) -> object:
     """Creates a new user"""
     password_hash = bcrypt.generate_password_hash(password)
-    User.query.add_column((username, password_hash))
+    user: User = User(username=username, password=password_hash)
+    db.session.add(user)
+    db.session.commit()
 
 
 def delete_user(user_id: int) -> object:
     """Delete a user by ID"""
     User.query.filter(User.id == user_id).delete()
+
+
+def user_auth(username: str, password: str) -> int | None:
+    """Check if the user exists"""
+    user: User = (User.query
+        .filter(User.username == username)
+        .one_or_none())
+    if user is not None and bcrypt.check_password_hash(user.password, password):
+        return user.id
+    
+def username_taken(username: str) -> bool:
+    """Check if the username is already taken or not"""
+    count: int = User.query.filter(User.username == username).count()
+    return count != 0
