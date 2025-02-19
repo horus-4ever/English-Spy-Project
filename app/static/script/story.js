a4947214a0acc33cf8b0309f6cc7580b5cb325c7
SIMONNEAU Lucas <lucas.simonneau@telecomnancy.eu>
2025-02-19 14:24:42 +0100

    reading a story is now possible

--- app/static/script/story.js
+++ app/static/script/story.js
@@ -1,5 +1,5 @@
 async function nextSentence(){
-    const url = "http://127.0.0.1:5000/api/stories/nodes/1/edges" //+ String(number);
+    const url = "http://127.0.0.1:5000/api/stories/1/nodes"; //+ String(number);
     console.log(url);
     const response = await fetch(url);
     if(!response.ok) {
@@ -9,39 +9,50 @@ async function nextSentence(){
     
     const content = document.querySelector(".text");
     try {
-        content.innerHTML = json.data.content;
+        content.innerHTML = json.nodes[number-1].content;
     } catch {
-        console.error("JSON error in field json.data.content\n Got " + json.data.content);
+        console.error("JSON error in field json.nodes[number-1].content\n Got " + json.nodes[number-1].content);
     }
     
     const left_img = document.querySelector("#p1");
     try {
-        left_img.src = json.data.left_img; 
+        left_img.src = json.nodes[number-1].left_img; 
     } catch {
-        console.error("JSON error in field json.data.left_img\n Got " + json.data.left_img);
+        console.error("JSON error in field json.nodes[number-1].left_img\n Got " + json.nodes[number-1].left_img);
     }
 
 
     const right_img = document.querySelector("#p2");
     try {
-        right_img.src = json.data.right_img;
+        right_img.src = json.nodes[number-1].right_img;
     } catch {
-        console.error("JSON error in field json.data.right_img\n Got " + json.data.right_img);
+        console.error("JSON error in field json.nodes[number-1].right_img\n Got " + json.nodes[number-1].right_img);
     }
 
     const speaker = document.querySelector(".speaker");
     try {
-        speaker.innerHTML = json.data.speaker;
+        speaker.innerHTML = json.nodes[number-1].speaker;
     } catch {
-        console.error("JSON error in field json.data.speaker\n Got " + json.data.speaker);
+        console.error("JSON error in field json.nodes[number-1].speaker\n Got " + json.nodes[number-1].speaker);
     }
-    try {
-        document.body.style.backgroundImage = url(json.data.background);
+    /*try {
+        document.body.style.backgroundImage = url(json.nodes[number-1].background);
     } catch {
-        console.error("JSON error in field json.data.background\n Got " + json.data.background);
-    }
-    if (json.data.type != "END"){
-        number = json.next[0].to;
-        console.log(number);
+        console.error("JSON error in field json.data.background\n Got " + json.nodes[number-1].background);
+    }*/
+    if (json.nodes[number-1].type != "END"){
+        const url_edges = "http://127.0.0.1:5000/api/stories/1/edges";
+        const rep = await fetch(url_edges);
+        if (!rep.ok){
+            throw new Error("HTTP error " + rep.status);
+        }
+        json_number = await rep.json();
+        for (let i=0;i<json_number.edges.length;i++){
+            if (json_number.edges[i].from  == number){
+                number = json_number.edges[i].to;
+                break;
+            }
+        }
+        console.log("number = ",number);
     }
 }
